e26a0c40f07b1c703552eb11b1d763ae877db71c
Varato <imxinchen@outlook.com>
2020-08-08 00:52:22 +0800

    CMakeLists updated

--- setup.py
+++ setup.py
@@ -1,40 +1,6 @@
 from skbuild import setup
 from setuptools import find_packages
 
-# ext_modules = []
-# data_files = []
-
-# if platform.system() == "Windows" and platform.machine() == "AMD64":
-#     internal_lib_dir = "lib_x86_64-win32"
-#     internal_include_dir = "emsim/ext/include"
-#     dlls = ["libfftw3f-3.dll"]
-#     data_files.append(("emsim/ext", [internal_lib_dir + '/' + dll for dll in dlls]))
-#
-#     dens_kernel = Extension(
-#         "emsim.ext.dens_kernel",
-#         sources=[
-#             "emsim/ext/src/dens_kernel.c",
-#             "emsim/ext/src/dens_kernel_pymodule.c"],
-#         include_dirs=[internal_include_dir, np.get_include()],
-#         library_dirs=[internal_lib_dir],
-#         libraries=['libfftw3f-3'],  # using single float fftw: change fftw_ to fftwf_ in c files
-#         extra_compile_args=['/openmp'],
-#     )
-#     ext_modules.append(dens_kernel)
-#
-#     em_kernel = Extension(
-#         "emsim.ext.em_kernel",
-#         sources=[
-#             "emsim/ext/src/em_kernel.c",
-#             "emsim/ext/src/em_kernel_pymodule.c"],
-#         include_dirs=[internal_include_dir, np.get_include()],
-#         library_dirs=[internal_lib_dir],
-#         libraries=['libfftw3f-3'],  # using single float fftw: change fftw_ to fftwf_ in c files
-#         extra_compile_args=['/openmp'],
-#     )
-#     ext_modules.append(em_kernel)
-
-
 setup(
     name="emsim",
     version="0.0.0",
@@ -47,5 +13,4 @@ setup(
         'atom_params': ['emsim/assets/*.txt'],
         'cuda_kernels': ['emsim/cuda/kernels/*.cu']
     },
-    # data_files=data_files
 )
